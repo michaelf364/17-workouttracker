28e3906b1593858270037573413fc195b9527e40
Michael Farris <farris.michael.j@gmail.com>
2020-07-28 19:38:03 -0700

    added html routes

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
 
-app.use("/api", require("./routes/api-routes"));
-app.use("/", require("./routes/html-routes"));
+app.use("/api", require("./routes/api-routes.js"));
+app.use("/", require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
